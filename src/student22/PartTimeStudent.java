e7643bf4cd3ad6355d3db3eefeac659dc532fbbf
colin teasdale <teasdaco@sheridancollege.ca>
2022-05-24 14:07:09 -0400

    Added Part Time Student Branch

---
+++ src/student22/PartTimeStudent.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package student22;
+
+/**
+ *
+ * SYST17796 Fundamentals of Software Design and Development
+ * @author Colin Teasdale   
+ */
+public class PartTimeStudent extends Student{
+    private int numOfCourses; 
+    
+    public int getNumOfCourses() {
+        return numOfCourses;
+    }
+
+    public void setNumOfCourses(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+
+}
